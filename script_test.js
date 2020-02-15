29e44469313a2a2d71c15ccb7d96341507034b13
RyanGuoTP <ryan.guo@thinkpower-info.com>
2020-02-15 18:05:31 +0800

    練習

--- script_test.js
+++ script_test.js
@@ -6,6 +6,29 @@ const app = new Vue({
         company: 'Thinkpower Engineer',
         day: '2020/02/15 by Ryan',
         signBox: 'sign',
-        imgSrc: '/images/main.png'
+        imgSrc: '/images/main.png',
+        changeMessage: '',
+        newMessage: '',
+        newColor: '',
+        todos: [
+            {text: 'Ryan Guo'},
+            {text: 'ThinkPower'},
+            {text: 'YaYaYa'}
+        ],
+        showAngular: true,
+        showVue: false
+    },
+    methods: {
+        onSubmit(){
+            this.company = this.changeMessage
+            this.changeMessage = ''
+        },
+        nameSubmit(){
+            this.name = this.newMessage
+            this.newMessage = ''
+        },
+        re(){
+            this.newColor = ''
+        }
     }
 })
