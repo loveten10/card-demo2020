1829b8b3db12e6b6b924ebfe8d9853444ceaf54f
RyanGuoTP <ryan.guo@thinkpower-info.com>
2020-02-15 15:18:16 +0800

    VUE版本

---
+++ script_test.js
@@ -0,0 +1,11 @@
+const app = new Vue({
+    el: '#app',
+
+    data: {
+        name: 'Ryan Guo',
+        company: 'Thinkpower Engineer',
+        day: '2020/02/15 by Ryan',
+        signBox: 'sign',
+        imgSrc: '/images/main.png'
+    }
+})
